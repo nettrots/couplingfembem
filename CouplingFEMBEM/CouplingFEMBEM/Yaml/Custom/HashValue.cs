0c587fb4f592ed29a18a41857961fefab2b57fa1
nettrots <nettrots@c5e9c2ba-2816-11de-a2f3-1373824ab735>
2009-04-23 07:44:21 +0000

    Yaml parser HashValue for our problem example of input file algorithm of filling input Hash

---
+++ CouplingFEMBEM/CouplingFEMBEM/Yaml/Custom/HashValue.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SbB.Diploma.Yaml.Custom
+{
+
+   
+    public struct HashValue
+    {
+        public double eDouble;
+        public int eInt;
+        public string eString;
+        public Dictionary<string, HashValue> eHash;
+
+    }
+
+}
