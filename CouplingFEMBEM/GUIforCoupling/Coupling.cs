8ea320f67288fe1b526910562cd2fc1814ae6c91
nettrots <nettrots@c5e9c2ba-2816-11de-a2f3-1373824ab735>
2009-05-11 23:02:48 +0000

    implicit FEMElement to Polygon added classes for graphic, triangulation, segments, domain and contours added

---
+++ CouplingFEMBEM/GUIforCoupling/Coupling.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ChartDirector;
+using SbB.Diploma;
+using SbB.Diploma.Methods;
+
+namespace GUIforCoupling
+{
+    public partial class Coupling : Form
+    {
+        private BaseChart chart;
+        private List<Graphic> graphs;
+        private DomainTriangulation dt;
+        private DomainTriangulation dt2;
+        private DomainTriangulation dt3;
+
+        public Coupling()
+        {
+            graphs=new List<Graphic>();
+            Polygon p =
+           new Polygon(new Vertex[] {new Vertex(0, 0), new Vertex(0, 1), new Vertex(1, 1), new Vertex(1, 0),});
+            GraphicOptions gropt=new GraphicOptions(FunctionType.ConstX,p) {ConstValue = 0.5};
+            Graphic g=new Graphic((x,y)=>-2*y*y+1) {N = 100, Name = "Name1", Options = gropt};
+
+            graphs.Add(g);
+
+            Polygon p1 =
+                new Polygon(new [] { new Vertex(0, 0), new Vertex(0, 1), new Vertex(1, 1), new Vertex(1, 0), });
+            GraphicOptions gropt1 = new GraphicOptions(FunctionType.InSegments, p1)
+                                        {
+                                            X1 = new Vertex(0, 0),
+                                            X2 = new Vertex(1, 1)
+                                        };
+            Graphic g1 = new Graphic((x, y) => x * y) {N = 100, Name = "Name2", Options = gropt1};
+            graphs.Add(g1);
+
+
+            LinearTriangle[] lintriangles=new LinearTriangle[3];
+            lintriangles[0]=new LinearTriangle(new Vertex(0,0),new Vertex(0.5,0),new Vertex(0,1) );
+            lintriangles[1] = new LinearTriangle(new Vertex(0.5, 0), new Vertex( 0.5,1), new Vertex(0.5, 0.5));
+            lintriangles[2] = new LinearTriangle(new Vertex(0, 1), new Vertex(0.5, 0.5), new Vertex(0.5, 1));
+
+            LinearBEMEdge[] segments = new LinearBEMEdge[8];
+            segments[0] = new LinearBEMEdge(new Vertex(0.5, 0), new Vertex(0.5, 0.1));
+            segments[1] = new LinearBEMEdge(new Vertex(0.5, 0.1), new Vertex(0.5, 0.2));
+            segments[2] = new LinearBEMEdge(new Vertex(0.5, 0.2), new Vertex(0.5, 0.4));
+            segments[3] = new LinearBEMEdge(new Vertex(0.5, 0.4), new Vertex(0.5, 0.6));
+            segments[4] = new LinearBEMEdge(new Vertex(0.5, 0.6), new Vertex(0.5, 0.8));
+            segments[5] = new LinearBEMEdge(new Vertex(0.5, 0.8), new Vertex(0.5, 1));
+            segments[6] = new LinearBEMEdge(new Vertex(0.5, 1), new Vertex(1, 1));
+            segments[7] = new LinearBEMEdge(new Vertex(1, 1), new Vertex(0, 1));
+
+            dt=new DomainTriangulation{Polygon=p};
+            dt2 = new DomainTriangulation { Elements = lintriangles };
+            dt3 = new DomainTriangulation() {Segments = segments};
+
+            InitializeComponent();
+            //lineModeChart();
+            triangulationeModeChart();
+            flash();
+            
+
+        }
+        private void flash()
+        {
+            Canvas.Image = chart.makeImage();
+
+        }
+
+        private void lineModeChart()
+        {
+
+            var chart1 = new XYChart(Canvas.Width, Canvas.Height);
+            chart1.setSize(Canvas.Width, Canvas.Height);
+            chart1.setRoundedFrame();
+
+            chart1.setPlotArea(30, 30, Canvas.Width - 60, Canvas.Height - 100, 0xffffff, -1, -1, 0xcccccc, 0xcccccc);
+            foreach (var graphic in graphs)
+            {
+                graphic.drawLine(chart1);
+            }
+
+            chart = chart1;
+
+            chart.addLegend(50, 30, false, "Arial Bold", 9).setBackground(
+                Chart.Transparent);
+
+
+        }
+
+        private void triangulationeModeChart()
+        {
+
+            var chart1 = new XYChart(Canvas.Width, Canvas.Height);
+            chart1.setSize(Canvas.Width, Canvas.Height);
+            chart1.setRoundedFrame();
+
+            chart1.setPlotArea(40, 40, Canvas.Width - 60, Canvas.Height - 100, 0xffffff, -1, -1, 0xffffff, 0xffffff);
+            dt.drawDomain(chart1);
+            dt2.triangulationLayer(chart1);
+            dt3.segmentsLayer(chart1);
+            chart = chart1;
+            //chart1.setShadingMode(Chart.S)
+
+        }
+
+        private void surfaceModeChart()
+        {
+            SurfaceChart chart1 = new SurfaceChart(Canvas.Width, Canvas.Height, 0xffffff, 0x888888);
+            chart1.setSize(Canvas.Width, Canvas.Height);
+            chart1.setRoundedFrame();
+            chart1.setPlotRegion(40, 40, Canvas.Width - 60, Canvas.Height - 100, 150);
+            chart1.setViewAngle(90);
+            chart1.setWallThickness(0);
+            chart1.setPerspective(0);
+
+            //chart1.setShadingMode(Chart.S)
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            
+            lineModeChart();
+            flash();
+            //Canvas.Invalidate();
+        }
+        
+    }
+
+   
+
+}
---
+++ CouplingFEMBEM/GUIforCoupling/Domain/Domain.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using ChartDirector;
+using SbB.Diploma;
+
+namespace GUIforCoupling
+{
+    public class DomainTriangulation
+    {
+        public FEMElement[] Elements{ get; set;}
+        public Edge[] Segments{ get; set; }
+        public Polygon Polygon { get; set; }
+        public DomainTriangulation()
+        {
+        }
+
+
+        private void drawline(Edge e,XYChart chart)
+        {
+//            LineLayer layer = chart.addLineLayer2();
+//
+//            layer.setLineWidth(2);
+//            
+//            //  Y
+//            layer.addDataSet(y, -1);
+        }
+
+        private void polygonLayer(Polygon polygon, int symbol, int symbSize, XYChart chart)
+        {
+            polygonLayer(polygon, -1, symbol, symbSize, chart);
+        }
+        private void polygonLayer(Polygon polygon,int color, XYChart chart)
+        {
+            polygonLayer(polygon, color, 1, 3, chart);
+        }
+
+        private void polygonLayer(Polygon polygon, XYChart chart)
+        {
+            polygonLayer(polygon, -1, 1, 3, chart);
+        }
+
+        private void polygonLayer(Polygon polygon,int color,int symbol,int symbSize,XYChart chart)
+        {
+            int N = polygon.Count;
+            double[] x = new double[N + 1], y = new double[N + 1];
+            for (int i = 0; i < polygon.Count+1; i++)
+            {
+                x[i] = polygon[i].X;
+                y[i] = polygon[i].Y;
+            }
+            // Add a line layer to the chart
+            LineLayer layer = chart.addLineLayer2();
+            layer.setLineWidth(1);
+            //  X
+            layer.setXData(x);
+            //  Y
+            layer.addDataSet(y, color);
+            //Chart.CrossShape
+            //Chart.SquareSymbol
+            layer.getDataSet(0).setDataSymbol(symbol, symbSize);
+        }
+
+    
+        public void triangulationLayer(XYChart chart)
+        {
+            foreach (FEMElement element in Elements)
+            {
+                polygonLayer(element, 0xff00ff, Chart.SquareSymbol,3, chart);
+            }
+
+        }
+
+        public void segmentsLayer(XYChart chart)
+        {
+            var v=new List<Vertex>();
+            foreach (var edge in Segments)
+            {
+                for (var i = 0; i < 2; i++)
+                {
+                if(!v.Contains(edge[i]))
+                    v.Add(edge[i]);                    
+                }
+            }
+            Polygon poly=new Polygon(v.ToArray());
+            polygonLayer(poly, Chart.CrossShape(), 4, chart);
+        }
+
+        public void drawDomain(XYChart chart)
+        {
+            polygonLayer(Polygon, chart);
+            //Boundary conditioans using Vector charts
+        }
+
+    }
+  
+
+}
